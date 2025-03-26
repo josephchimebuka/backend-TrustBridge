e4820bbbec345d73a8ddd8f36b06736a7319fa44
KAMALDEEN333 <kamalaliyu503@gmail.com>
2025-03-26 09:41:27 +0100

    Create errorHandler.ts
    Added error handling in the middleware to make it global

---
+++ src/middleware/errorHandler.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+
+const errorHandler = (
+  err: Error & { status?: number }, // Extend Error to include status
+  req: Request,
+  res: Response,
+): void => {
+  console.error(err.stack); // Log the error details
+
+  // Send the error response
+  res.status(err.status || 500).json({
+    message: err.message || 'Internal Server Error',
+    // Include stack trace only in development
+    stack: process.env.NODE_ENV === 'development' ? err.stack : undefined,
+  });
+};
+
+export default errorHandler;
